962880f4702cc2ed8c68dc660778791a1faad7da
phiraphat <peel2aput@gmail.com>
2025-11-10 10:31:48 +0700

    fix : prefix path api

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,9 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  // Set global prefix
+  app.setGlobalPrefix("api");
+
   // Global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
